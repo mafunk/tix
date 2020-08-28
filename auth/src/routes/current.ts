d9a22bb5bde232bb3a088a75c86af33703c647a8
Michael Funk <mfunk@brandsource.com>
2020-08-27 18:30:27 -0700

    add current user endpoint

--- auth/src/routes/current.ts
+++ auth/src/routes/current.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { Password } from "../services/password";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+
+const JWT_SECRET = process.env.JWT_KEY!;
 
 const router = express.Router();
 
 router.get("/api/users/current", (req, res) => {
-  res.send("hi");
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, JWT_SECRET);
+
+    res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentRouter };
