f78969d2124c0096ad9924ff1249c3a17dacd075
Michael Funk <mfunk@brandsource.com>
2020-08-27 18:20:04 -0700

    add singin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,49 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { Password } from "../services/password";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+
+const JWT_SECRET = process.env.JWT_KEY!;
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Password required"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("email/password invalid");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError("password/email invalid");
+    }
+
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      JWT_SECRET
+    );
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
