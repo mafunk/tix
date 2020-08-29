a6f299df0e198978f0dfb59e9069b34eb1198b43
Michael Funk <mfunk@brandsource.com>
2020-08-29 12:07:36 -0700

    edit event

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,4 +1,5 @@
 import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
 
 export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
@@ -8,7 +9,7 @@ export interface OrderCreatedEvent {
     version: number;
 
     userId: string;
-    status: string;
+    status: OrderStatus;
     expiresAt: string;
 
     ticket: {
